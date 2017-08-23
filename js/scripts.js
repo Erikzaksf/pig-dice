46a054b22292499557db01a5f7a90da7b0734ec3
Erik Zakrzewski, Kristen Kulha and tyler miller <kristen@>
2017-08-23 10:42:43 -0700

    add basic user/business interface logic to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 // Business Logic
+function Player(score, turnScore){
+  this.score = score;
+  this.turnScore = turnScore;
+}
+
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -7,8 +12,17 @@ function getRandomIntInclusive(min, max) {
 
 
 
+
 // Interface Logic
 $(document).ready(function(){
+  $("#play").click(function(event){
+    var playerOne = new Player(0, 0);
+    var playerTwo = new Player(0, 0);
+  })
+  $("#rollOne").click(function(event){
+    var roll = getRandomIntInclusive(1,6);
+    console.log(roll);
+  });
 
 
 });
