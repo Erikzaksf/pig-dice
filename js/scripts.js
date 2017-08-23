b5c76f8420e6b99dd4f60a0568a175255c56c774
Erik Zakrzewski, Kristen Kulha and tyler miller <kristen@>
2017-08-23 13:47:35 -0700

    add current roll counter as well as game end functionality

--- js/scripts.js
+++ js/scripts.js
@@ -25,31 +25,64 @@ Player.prototype.remove = function(turnScore) {
 
 // Interface Logic
 $(document).ready(function(){
+  var diceRolls = [];
   var playerOne = new Player(0, 0);
   var playerTwo = new Player(0, 0);
   $("#play").click(function(event){
     $("#playerOneButtons").show();
   });
+// PLAYER ONE AREA
   $("#rollOne").click(function(event){
+    $("#playerTwoRolls").empty();
     var roll = getRandomIntInclusive(1,6);
     console.log(roll);
     if(roll === 1) {
+      $("#playerOneRolls").append("<h1>YOU PIGGED OUT</h1>");
       playerOne.remove(playerOne.turnScore);
       $("#playerOneButtons").hide();
       $("#playerTwoButtons").show();
     } else {
       playerOne.add(roll);
+      $("#playerOneRolls").append("<li>" + roll + "</li><br>");
     }
   });
   $("#holdOne").click(function(event){
     playerOne.hold(playerOne.turnScore);
+    $("#playerOneScore").text(playerOne.score);
     if(playerOne.score >= 100) {
-      // player one wins the game!
+      alert("Player One winner winner chicken dinner!");
     } else {
       $("#playerOneButtons").hide();
       $("#playerTwoButtons").show();
       playerOne.remove(playerOne.turnScore);
     }
     console.log(playerOne.score);
+  });
+// PLAYER TWO AREA
+  $("#rollTwo").click(function(event){
+    $("#playerOneRolls").empty();
+    var roll = getRandomIntInclusive(1,6);
+    console.log(roll);
+    if(roll === 1) {
+      $("#playerTwoRolls").append("<h1>YOU PIGGED OUT</h1>");
+      playerTwo.remove(playerTwo.turnScore);
+      $("#playerTwoButtons").hide();
+      $("#playerOneButtons").show();
+    } else {
+      playerTwo.add(roll);
+      $("#playerTwoRolls").append("<li>" + roll + "</li><br>");
+    }
+  });
+  $("#holdTwo").click(function(event){
+    playerTwo.hold(playerTwo.turnScore);
+    $("#playerTwoScore").text(playerTwo.score);
+    if(playerTwo.score >= 100) {
+      alert("Player Two winner winner chicken dinner!");
+    } else {
+      $("#playerTwoButtons").hide();
+      $("#playerOneButtons").show();
+      playerTwo.remove(playerTwo.turnScore);
+    }
+    console.log(playerTwo.score);
   });
 });
