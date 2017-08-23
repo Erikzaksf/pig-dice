0b5d10ac4319b5c5d4920d744ce580bc0fa6b98a
Erik Zakrzewski, Kristen Kulha and tyler miller <kristen@>
2017-08-23 10:18:35 -0700

    Add function to randomly generate number.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Business Logic
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+// Interface Logic
+$(document).ready(function(){
+
+
+});
