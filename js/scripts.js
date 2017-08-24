84738befb6ea7755e4c21ec484271d454a75b2b6
Erik Zakrzewski, Kristen Kulha and Tyler Miller <erik@>
2017-08-24 10:47:59 -0700

    Implement computer player that will hold after two rolls.

--- js/scripts.js
+++ js/scripts.js
@@ -37,55 +37,57 @@ $(document).ready(function(){
   $("#rollOne").click(function(event){
     $("#playerTwoRolls").empty();
     var roll = getRandomIntInclusive(1,6);
-    console.log(roll);
     if(roll === 1) {
       $("#playerOneRolls").append("<h1>YOU PIGGED OUT</h1>");
       playerOne.remove(playerOne.turnScore);
-      $("#playerOneButtons").hide();
-      $("#playerTwoButtons").show();
+      var roll = getRandomIntInclusive(1,6);
+      for (var i = 0; i < 2; i++) {
+        if(roll === 1) {
+          $("#playerTwoRolls").append("<h1>YOU PIGGED OUT</h1>");
+          playerTwo.remove(playerTwo.turnScore);
+          i = 2;
+        }else {
+          playerTwo.add(roll);
+          $("#playerTwoRolls").append("<li>" + roll + "</li><br>");
+        }
+        playerTwo.hold(playerTwo.turnScore);
+        $("#playerTwoScore").text(playerTwo.score);
+        playerTwo.remove(playerTwo.turnScore);
+      }
     } else {
       playerOne.add(roll);
       $("#playerOneRolls").append("<li>" + roll + "</li><br>");
     }
   });
+
   $("#holdOne").click(function(event){
     playerOne.hold(playerOne.turnScore);
+    $("#playerOneRolls").empty();
     $("#playerOneScore").text(playerOne.score);
     if(playerOne.score >= 100) {
       alert("Player One winner winner chicken dinner!");
     } else {
-      $("#playerOneButtons").hide();
-      $("#playerTwoButtons").show();
-      playerOne.remove(playerOne.turnScore);
-    }
-    console.log(playerOne.score);
-  });
-// PLAYER TWO AREA
-  $("#rollTwo").click(function(event){
-    $("#playerOneRolls").empty();
-    var roll = getRandomIntInclusive(1,6);
-    console.log(roll);
-    if(roll === 1) {
-      $("#playerTwoRolls").append("<h1>YOU PIGGED OUT</h1>");
-      playerTwo.remove(playerTwo.turnScore);
-      $("#playerTwoButtons").hide();
-      $("#playerOneButtons").show();
-    } else {
-      playerTwo.add(roll);
-      $("#playerTwoRolls").append("<li>" + roll + "</li><br>");
-    }
-  });
-  $("#holdTwo").click(function(event){
-    playerTwo.hold(playerTwo.turnScore);
-    $("#playerTwoScore").text(playerTwo.score);
-    if(playerTwo.score >= 100) {
-      alert("Player Two winner winner chicken dinner!");
-    } else {
-      $("#playerTwoButtons").hide();
-      $("#playerOneButtons").show();
-      playerTwo.remove(playerTwo.turnScore);
-    }
-    console.log(playerTwo.score);
+        playerOne.remove(playerOne.turnScore);
+        }
+        for (var i = 0; i < 2; i++) {
+          var roll = getRandomIntInclusive(1,6);
+          if(roll === 1) {
+            $("#playerTwoRolls").append("<h1>YOU PIGGED OUT</h1>");
+            playerTwo.remove(playerTwo.turnScore);
+            i = 2;
+          }else {
+            playerTwo.add(roll);
+            $("#playerTwoRolls").append("<li>" + roll + "</li><br>");
+          }
+        }
+        playerTwo.hold(playerTwo.turnScore);
+        playerTwo.remove(playerTwo.turnScore);
+        if (playerTwo.score >= 100) {
+          alert("Player Two winner winner chicken dinner!");
+        }else{
+          $("#playerTwoScore").text(playerTwo.score);
+        }
+
   });
 
 });
